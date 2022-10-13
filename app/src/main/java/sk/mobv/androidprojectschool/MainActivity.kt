0c258251b27a62220aa6239c243a62edc706c8d4
DESKTOP-CA4DVI4\eniko <enikovillant@gmail.com>
2022-10-14 01:59:41 +0200

    add fragments

--- app/src/main/java/sk/mobv/androidprojectschool/MainActivity.kt
+++ app/src/main/java/sk/mobv/androidprojectschool/MainActivity.kt
@@ -3,13 +3,17 @@ package sk.mobv.androidprojectschool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.airbnb.lottie.LottieAnimationView
+import sk.mobv.androidprojectschool.databinding.ActivityMainBinding
 
 //for binding
 //import sk.mobv.animation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navController: NavController
     // WITH BINDING
 
     //    lateinit var binding: ActivityMainBinding
@@ -24,17 +28,23 @@ class MainActivity : AppCompatActivity() {
 //    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            startAnimation()
-        }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
-    private fun startAnimation() {
-        val animationView: LottieAnimationView = findViewById(R.id.animation_view)
-        animationView.playAnimation()
-    }
+//    private fun startAnimation() {
+//        val animationView: LottieAnimationView = findViewById(R.id.animation_view)
+//        animationView.playAnimation()
+//    }
 
 }
